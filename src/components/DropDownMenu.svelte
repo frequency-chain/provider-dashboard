1c54012a5bf29eabd85148e4e6e23b93de241adc
Robert La Ferla <robert@onemsg.co>
2024-01-16 18:14:37 -0500

    feat: Svelte drop down menu component

---
+++ src/components/DropDownMenu.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let label: string;
+    export let id: string;
+    export let options: Record<string, string>;
+    export let selected: string;
+    export let onChange = () => {};
+    export let onSelect = () => {};
+    export let required = false;
+</script>
+
+<label class="section-title" for={id}>{label}</label>
+
+<div style="margin-bottom: 14px;"></div>
+
+<select id={id} bind:value={selected} {required} on:change={onChange} on:select={onSelect} class="dropdown">
+    {#each Object.keys(options) as option}
+    <option value={option} class="bg-base">{option}: {options[option]}</option>
+    {/each}
+</select>
