724d5650baf3337ce48292748eec96acfecfbcd0
Robert La Ferla <robert@onemsg.co>
2024-01-17 12:33:53 -0500

    fix: css

---
+++ src/components/DropDownMenu.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let label: string;
+    export let id: string = "";
+    export let options: Record<string, string>;
+    export let selected: string = "";
+    export let placeholder: string = "";
+    export let onChange = () => {};
+    export let onSelect = () => {};
+    export let required = false;
+  </script>
+  
+  <style>
+      option[value=""][disabled] {
+        display: none;
+      }
+  </style>
+  
+  <label class="label block mb-3.5" for={id}>{label}</label>
+  <select {id} bind:value={selected} {required} on:change={onChange} on:select={onSelect}>
+    {#if placeholder !== ""}
+        <option class="text-disabled" value="" disabled selected>{placeholder}</option>
+    {/if}
+    {#each Object.entries(options) as [key, value]}
+    <option value={key} class="bg-base">{key}: {value}</option>
+    {/each}
+  </select>
+  
