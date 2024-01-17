f1bc7e14f1648e2ddbcd02d8ff454517fc2c1b67
Robert La Ferla <robert@onemsg.co>
2024-01-17 10:06:06 -0500

    feat: added placeholder support

--- src/components/DropDownMenu.svelte
+++ src/components/DropDownMenu.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
-    export let label: string;
-    export let id: string;
-    export let options: Record<string, string>;
-    export let selected: string;
-    export let onChange = () => {};
-    export let onSelect = () => {};
-    export let required = false;
+  export let label: string;
+  export let id: string;
+  export let options: Record<string, string>;
+  export let selected: string;
+  export let placeholder: string;
+  export let onChange = () => {};
+  export let onSelect = () => {};
+  export let required = false;
 </script>
 
 <label class="section-title" for={id}>{label}</label>
 
 <div style="margin-bottom: 14px;"></div>
 
-<select id={id} bind:value={selected} {required} on:change={onChange} on:select={onSelect} class="dropdown">
-    {#each Object.keys(options) as option}
+<select {id} bind:value={selected} {required} on:change={onChange} on:select={onSelect} class="dropdown">
+  {#if placeholder}
+    <option value="" disabled selected>{placeholder}</option>
+  {/if}
+  {#each Object.keys(options) as option}
     <option value={option} class="bg-base">{option}: {options[option]}</option>
-    {/each}
+  {/each}
 </select>
