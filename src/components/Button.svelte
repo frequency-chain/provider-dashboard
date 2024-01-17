0dfda064a3f5bf5ebc6b13c612a76fab268e6377
Robert La Ferla <robert@onemsg.co>
2024-01-17 15:19:23 -0500

    feat: Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let title: string = '';
+  let action = () => {};
+</script>
+
+<div class="flex w-350 justify-between">
+  <button on:click|preventDefault={action} class="btn-primary">{title}</button>
+</div>
