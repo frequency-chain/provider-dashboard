1346784ec008a20d208924e2d5f0b2e5e12fc17d
Robert La Ferla <robert@onemsg.co>
2024-01-18 11:00:25 -0500

    fix: added block sections and buttons

--- src/components/BlockSection.svelte
+++ src/components/BlockSection.svelte
@@ -2,7 +2,7 @@
   export let title: string;
 </script>
 
-<div class="content-block w-single-block flex flex-col gap-4">
+<div class="content-block flex flex-col gap-4">
   <h2 class="section-title-underlined">{title}</h2>
   <slot />
 </div>
