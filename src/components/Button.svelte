edbe0b6af0ec6b00853fb4c119ac9127f9911cb0
Robert La Ferla <robert@onemsg.co>
2024-01-18 10:30:30 -0500

    feat: Reusable Button component (#101)

---
+++ src/components/Button.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let title: string = '';
+  export let action = () => {};
+</script>
+
+<button on:click|preventDefault={action} class="btn-primary" {...$$restProps}>{title}</button>
