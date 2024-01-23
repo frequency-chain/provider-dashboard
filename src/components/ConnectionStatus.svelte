d6d279412e5cc4d6fb3bf5633a8e4c19d084e55a
Robert La Ferla <robert@onemsg.co>
2024-01-23 13:07:43 -0500

    fix: updated connection status (WIP)

--- src/components/ConnectionStatus.svelte
+++ src/components/ConnectionStatus.svelte
@@ -10,6 +10,10 @@
     networkEndpointURL = networkToInfo[value].endpoint?.toString()?.replace(/\/$/, '');
     });
 
+function switchNetwork() {
+  console.log('switchNetwork');
+}
+
 </script>
 
 <div class="flex gap-6 items-center" class:hidden={!$isLoggedIn}>
@@ -23,10 +27,6 @@
 
   <!-- TODO: CHANGE ICON WHEN APPROVED -->
   <!-- TODO: ADD ONCLICK WHEN BUILDING SWITCH MODAL -->
-  <div style="width: 100%; height: 100%; background: #6EE9D1; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 5px"><img src={networkSwitchIcon} alt="Switch Network" /></div>
-  
-  <button on:click={() => {}}>
-    
-  </button>
+  <button on:click={switchNetwork} class="flex justify-center items-center select-none bg-red-500 border-2 text-xl font-bold p-2 m-2 rounded-full shadow h-20 w-20 focus:outline-none focus:shadow-outline" style="width: 44px; height: 44px; background: #6EE9D1; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 5px"><img class="" src={networkSwitchIcon} alt="Switch Network" /></button>
   <!-- {/if} -->
 </div>
