ecce9228411c8b31e4a14a31291fc19231e2bdee
Robert La Ferla <robert@onemsg.co>
2024-01-23 13:22:21 -0500

    fix: cleaned up some styling (WIP)

--- src/components/ConnectionStatus.svelte
+++ src/components/ConnectionStatus.svelte
@@ -20,13 +20,13 @@ function switchNetwork() {
   <!-- {#if connected} -->
   <!-- TODO: ADD REAL DATA WHEN STORE IS CONNECTED -->
   <div class="flex gap-2 items-center">
-    <div class="bg-red-error h-1 w-1 p-1 rounded-full" />
+    <div class="bg-green-success h-1 w-1 p-1 rounded-full" />
     <p class="text-md uppercase">{networkName}</p>
   </div>
   <p class="text-md">{networkEndpointURL}</p>
 
   <!-- TODO: CHANGE ICON WHEN APPROVED -->
   <!-- TODO: ADD ONCLICK WHEN BUILDING SWITCH MODAL -->
-  <button on:click={switchNetwork} class="flex justify-center items-center select-none bg-red-500 border-2 text-xl font-bold p-2 m-2 rounded-full shadow h-20 w-20 focus:outline-none focus:shadow-outline" style="width: 44px; height: 44px; background: #6EE9D1; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 5px"><img class="" src={networkSwitchIcon} alt="Switch Network" /></button>
+  <button on:click={switchNetwork} class="flex justify-center items-center select-none p-2 m-2 rounded-full shadow h-20 w-20 focus:outline-none focus:shadow-outline" style="width: 44px; height: 44px; background: #6EE9D1; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 5px"><img class="" src={networkSwitchIcon} alt="Switch Network" /></button>
   <!-- {/if} -->
 </div>
