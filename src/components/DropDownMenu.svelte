a3571be14f6cbbc2a92073a5ec6c12150ee0824c
Robert La Ferla <robert@onemsg.co>
2024-01-25 09:20:53 -0500

    fix: default action is do nothing

--- src/components/DropDownMenu.svelte
+++ src/components/DropDownMenu.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   export let label: string;
   export let id: string = '';
-  export let action: any;
+  export let action = () => {};
   export let options: Record<string, string>;
   export let placeholder: string = '';
   export let onChange = () => {};
