09cf5292e324881d76494b8ad9993447957e5222
Robert La Ferla <robert@onemsg.co>
2024-01-26 20:37:21 -0500

    fix: Cleanup from PR comments

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -3,21 +3,13 @@
   import Provider from '$components/Provider.svelte';
   import DashboardHeader from '$components/DashboardHeader.svelte';
   import ChainStatus from '$components/ChainStatus.svelte';
-  import type { ChainInfo } from '$lib/storeTypes';
-
   import { storeBlockNumber, storeChainInfo, storeConnected, storeToken } from '$lib/stores';
 
+  let blockNumber = $storeBlockNumber;
+  let epochNumber = $storeChainInfo.epochNumber;
+  let connected = $storeConnected;
   let token = '';
-
   storeToken.subscribe((val) => (token = val));
-  let blockNumber = 0n;
-  let epochNumber = 0n;
-  let connected = false;
-
-  // TODO: put all this in chainInfo and update how it's stored.
-  storeBlockNumber.subscribe((val) => (blockNumber = val));
-  storeConnected.subscribe((val) => (connected = val));
-  storeChainInfo.subscribe((info: ChainInfo) => (epochNumber = info.epochNumber));
 </script>
 
 <div class="flex flex-col gap-4">
