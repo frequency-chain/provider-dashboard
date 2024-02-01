b92dde98eb5708440d8d0ad31cfe43a217d2cda2
Claire Olmstead <olmsteadclaire@gmail.com>
2024-02-01 12:51:25 -0800

    extract FAQ Item

---
+++ src/components/FAQItem.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  let isOpen = false;
+
+  function toggleIsOpen() {
+    isOpen = !isOpen;
+  }
+</script>
+
+<button
+  id="faq-question"
+  class="label text-md mt-2 flex w-full items-center justify-between border-t border-divider p-4 text-left"
+  on:click={toggleIsOpen}
+>
+  <slot name="question" />
+  <svg
+    class="w-8 ml-12 h-8 shrink-0 transform rounded bg-blue p-1 transition-transform duration-300 {isOpen
+      ? 'rotate-180'
+      : ''}"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+      clip-rule="evenodd"
+    />
+  </svg>
+</button>
+
+<div id="faq-answer" class={`p-3 ${isOpen ? 'block' : 'hidden'}`}>
+  <slot name="answer" />
+</div>
--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,49 +1,60 @@
 <script lang="ts">
-  import MainnetVsTestnet from '$components/FaqAnswers/MainnetVsTestnet.svelte';
-  import TransactAsProvider from '$components/FaqAnswers/TransactAsProvider.svelte';
-  import type { ComponentType } from 'svelte';
-
-  let openItem: null | number = null;
-
-  const questions: { q: string; a: ComponentType }[] = [
-    {
-      q: 'What is the difference between Mainnet and Testnet (Rococo)?',
-      a: MainnetVsTestnet,
-    },
-    {
-      q: 'How do I transact on Frequency as a provider?',
-      a: TransactAsProvider,
-    },
-  ];
+  import FAQItem from '$components/FAQItem.svelte';
 </script>
 
 <div class="content-block w-single-block text-left text-base">
   <h2 class="section-title">FAQ's</h2>
-  {#each questions as item, index}
-    <button
-      id="faq-question"
-      class="label text-md mt-2 flex w-full items-center justify-between border-t border-divider p-4 text-left"
-      on:click={() => (openItem = openItem === index ? null : index)}
-    >
-      {item.q}
-      <svg
-        class="w-8 ml-12 h-8 shrink-0 transform rounded bg-blue p-1 transition-transform duration-300 {openItem ===
-        index
-          ? 'rotate-180'
-          : ''}"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-          clip-rule="evenodd"
-        />
-      </svg>
-    </button>
-    <div id="faq-answer" class={`p-3 ${openItem === index ? 'block' : 'hidden'}`}>
-      <svelte:component this={item.a} />
+
+  <FAQItem>
+    <span slot="question">What is the difference between Mainnet and Testnet (Rococo)?</span>
+    <div slot="answer" class="column">
+      <p>
+        The Frequency Mainnet is the production Frequency blockchain network. The Frequency Rococo Testnet, which works
+        with the Polkadot Rococo Testnet, is for developers to test and debug applications without risking real assets.
+      </p>
+      <p>What about the other options?</p>
+      <ul class="unordered-list">
+        <li>To connect to a node running on your desktop, choose Localhost.</li>
+        <li>
+          To connect to a node that is not in the list, choose Other, then type the desired WebSocket address in the
+          text field.
+        </li>
+      </ul>
     </div>
-  {/each}
+  </FAQItem>
+
+  <FAQItem>
+    <span slot="question">How do I transact on Frequency as a provider?</span>
+    <div slot="answer" class="column">
+      <p>
+        To transact on Frequency as a provider you will need frequency utility tokens. On Frequency testnet, you can get
+        tokens from the Testnet Faucet.
+      </p>
+      <p>To do that:</p>
+      <ul class="unordered-list">
+        <li>
+          <a href="https://faucet.rococo.frequency.xyz/" target="_blank" rel="noopener noreferrer" class="underline">
+            Get XRQCY tokens for Frequency Testnet (Rococo)
+          </a>
+          and follow the instructions using your desired wallet address to get XRQCY tokens.
+        </li>
+        <li>
+          Once that succeeds, verify the tokens have made it to your wallet by selecting or re-selecting the address
+          above. You may need to wait a minute.
+        </li>
+        <li>
+          For more information, you can also visit the
+          <a
+            href="https://cloudflare-ipfs.com/ipns/dotapps.io/#/accounts"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="underline"
+          >
+            Rococo Accounts page via the Polkadot UI.
+          </a>
+          .
+        </li>
+      </ul>
+    </div>
+  </FAQItem>
 </div>
