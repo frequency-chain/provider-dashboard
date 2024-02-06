d908a58517c6a1dfdc4bc1df9fe51ebbcf11ddf4
V. Claire Olmstead <43625033+claireolmstead@users.noreply.github.com>
2024-02-06 08:54:32 -0800

    create reusable component for how to transact. add reusable styling. (#134)
    closes #127
    
    ---------
    
    Co-authored-by: Claire Olmstead <olmsteadclaire@gmail.com>

---
+++ src/components/HowToTransact.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let additionalStyles: string = '';
+</script>
+
+<div class={`column ${additionalStyles}`}>
+  <p>
+    To transact on Frequency as a provider you will need Frequency utility tokens. On Frequency Testnet, you can get
+    tokens from the Testnet Faucet.
+  </p>
+  <p>To do that:</p>
+  <ol class="ordered-list">
+    <li>
+      <a href="https://faucet.rococo.frequency.xyz/" target="_blank" rel="noopener noreferrer" class="underline">
+        Get XRQCY tokens for Frequency Testnet (Rococo)
+      </a>
+      and follow the instructions using your desired wallet address to get XRQCY tokens.
+    </li>
+    <li>
+      Once that succeeds, verify the tokens have made it to your wallet by selecting or re-selecting the address above.
+      You may need to wait a minute.
+    </li>
+    <li>
+      For more information, you can also visit the
+      <a
+        href="https://cloudflare-ipfs.com/ipns/dotapps.io/#/accounts"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="underline"
+      >
+        Rococo Accounts page via the Polkadot UI.
+      </a>
+      .
+    </li>
+  </ol>
+</div>
--- src/components/ProviderLogin.svelte
+++ src/components/ProviderLogin.svelte
@@ -6,6 +6,7 @@
   import BlockSection from '$components/BlockSection.svelte';
   import SelectNetworkAndAccount from './SelectNetworkAndAccount.svelte';
   import Button from '$components/Button.svelte';
+  import HowToTransact from '$components/HowToTransact.svelte';
   import { pageContent } from '$lib/stores/pageContentStore';
   import { providerAccountsStore } from '$lib/stores/accountsStore';
   import { user } from '$lib/stores/userStore';
@@ -49,11 +50,5 @@
     <Button id="request-2b-provider-btn" title="Become a Provider" action={becomeProvider} />
   </BlockSection>
 
-  <div class="mt-24 text-xs">
-    To transact on Frequency as a provider you will need frequency utility tokens. On Frequency testnet, you can get
-    tokens from the Testnet Faucet. To do that: Get XRQCY tokens for Frequency Testnet (Rococo) and follow the
-    instructions using your desired wallet address to get XRQCY tokens. Once that succeeds, verify the tokens have made
-    it to your wallet by selecting or re-selecting the address above. You may need to wait a minute. For more
-    information, you can also visit the Rococo Accounts page via the Polkadot UI.
-  </div>
+  <HowToTransact additionalStyles="mt-24 text-xs gap-1" />
 </div>
