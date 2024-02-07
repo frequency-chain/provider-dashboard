d01c81f5b64fafb93ed904f895663d3579c7cdf5
Claire Olmstead <olmsteadclaire@gmail.com>
2024-02-07 14:10:48 -0800

    logout

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,18 +1,40 @@
 <script lang="ts">
+  import { user } from '$lib/stores/userStore';
+  import { Account } from '$lib/stores/accountsStore';
+  import { dotApi } from '$lib/stores';
   import { onMount } from 'svelte';
   import iconLogo from '$lib/assets/icon-logo.png';
+  import { defaultDotApi } from '$lib/storeTypes';
+  import { pageContent } from '$lib/stores/pageContentStore';
 
   let url = '';
   onMount(() => (url = window.location.pathname));
 
-  $: navItems = [
+  let navItems = [
     { name: 'Home', href: '/', isActive: url === '/' },
     { name: "FAQ's", href: '/faq', isActive: url === '/faq' },
     { name: 'Logout', href: '', isActive: false },
   ];
 
-  //TODO: logout in store once store created
-  const logout = () => {};
+  function showActive(curItem: { name: string; href: string; isActive: boolean }) {
+    const items = navItems.map((item) => {
+      return { ...item, isActive: item.name === curItem.name };
+    });
+    navItems = items;
+  }
+
+  function logout() {
+    $user = new Account();
+    $dotApi = defaultDotApi;
+    pageContent.login();
+  }
+
+  function handleClick(curItem: { name: string; href: string; isActive: boolean }) {
+    showActive(curItem);
+    if (curItem.name === 'Logout') {
+      logout();
+    }
+  }
 </script>
 
 <div class="fixed flex h-screen w-[126px] min-w-[126px] flex-col items-center justify-center bg-bg-black">
@@ -20,12 +42,7 @@
   <div class="flex w-[100%] flex-col">
     {#each navItems as navItem}
       <a
-        on:click={() => {
-          const items = navItems.map((item) => {
-            return { ...item, isActive: item.name === navItem.name };
-          });
-          navItems = items;
-        }}
+        on:click|preventDefault={() => handleClick(navItem)}
         href={navItem.href}
         class={` flex h-[100px] items-center justify-center text-sm font-bold ${
           navItem.isActive && 'bg-bg-black-active shadow-blue-border'
