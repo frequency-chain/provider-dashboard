d5eb9d949a668b771be690555f9238209fdde780
V. Claire Olmstead <43625033+claireolmstead@users.noreply.github.com>
2024-02-08 10:55:26 -0800

    121 logout button (#147)
    closes #121 
    
    ## Changes
    - Improved logic of `Nav` component
    - Created `NavItem` component 
    - Created `logout` function in `user` store
    
    ## How to test
    - Should not see logout button prior to login
    - On login see logout button
    - Logout button clears store and takes you back to login page
    - Loggin back in should work as expected
    - Navigation should work as expected
    
    Note: State does not persist yet so going navigating to FAQ will clear
    the stores.
    
    ---------
    
    Co-authored-by: Claire Olmstead <olmsteadclaire@gmail.com>

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,36 +1,21 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import { logout } from '$lib/stores/userStore';
+  import { isLoggedIn } from '$lib/stores';
   import iconLogo from '$lib/assets/icon-logo.png';
+  import { onMount } from 'svelte';
+  import NavItem from '$components/NavItem.svelte';
 
-  let url = '';
+  let url = '/';
   onMount(() => (url = window.location.pathname));
-
-  $: navItems = [
-    { name: 'Home', href: '/', isActive: url === '/' },
-    { name: "FAQ's", href: '/faq', isActive: url === '/faq' },
-    { name: 'Logout', href: '', isActive: false },
-  ];
-
-  //TODO: logout in store once store created
-  const logout = () => {};
 </script>
 
 <div class="fixed flex h-screen w-[126px] min-w-[126px] flex-col items-center justify-center bg-bg-black">
   <img alt="iconLogo" src={iconLogo} class="absolute left-6 top-7 w-[57px]" />
   <div class="flex w-[100%] flex-col">
-    {#each navItems as navItem}
-      <a
-        on:click={() => {
-          const items = navItems.map((item) => {
-            return { ...item, isActive: item.name === navItem.name };
-          });
-          navItems = items;
-        }}
-        href={navItem.href}
-        class={` flex h-[100px] items-center justify-center text-sm font-bold ${
-          navItem.isActive && 'bg-bg-black-active shadow-blue-border'
-        }`}>{navItem.name}</a
-      >
-    {/each}
+    <NavItem href="/" isActive={url === '/'}>Home</NavItem>
+    <NavItem href="/faq" isActive={url === '/faq'}>FAQ's</NavItem>
+    {#if $isLoggedIn}
+      <NavItem href="/" onClick={logout}>Logout</NavItem>
+    {/if}
   </div>
 </div>
---
+++ src/components/NavItem.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let href: string = '';
+  export let isActive: boolean = false;
+  export let onClick: () => void = () => {};
+</script>
+
+<a
+  {href}
+  on:click|stopPropagation={onClick}
+  class={` flex h-[100px] items-center justify-center text-sm font-bold ${
+    isActive && 'bg-bg-black-active shadow-blue-border'
+  }`}
+>
+  <slot />
+</a>
