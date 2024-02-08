a821bf11e6a5e32ef01e45e49afddd827bc23eb0
Claire Olmstead <olmsteadclaire@gmail.com>
2024-02-08 08:04:58 -0800

    logout button created and nav component improved

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,53 +1,21 @@
 <script lang="ts">
-  import { user } from '$lib/stores/userStore';
-  import { Account } from '$lib/stores/accountsStore';
-  import { dotApi } from '$lib/stores';
-  import { onMount } from 'svelte';
+  import { logout } from '$lib/stores/userStore';
+  import { isLoggedIn } from '$lib/stores';
   import iconLogo from '$lib/assets/icon-logo.png';
-  import { defaultDotApi } from '$lib/storeTypes';
-  import { pageContent } from '$lib/stores/pageContentStore';
+  import { onMount } from 'svelte';
+  import NavItem from '$components/NavItem.svelte';
 
-  let url = '';
+  let url = '/';
   onMount(() => (url = window.location.pathname));
-
-  let navItems = [
-    { name: 'Home', href: '/', isActive: url === '/' },
-    { name: "FAQ's", href: '/faq', isActive: url === '/faq' },
-    { name: 'Logout', href: '', isActive: false },
-  ];
-
-  function showActive(curItem: { name: string; href: string; isActive: boolean }) {
-    const items = navItems.map((item) => {
-      return { ...item, isActive: item.name === curItem.name };
-    });
-    navItems = items;
-  }
-
-  function logout() {
-    $user = new Account();
-    $dotApi = defaultDotApi;
-    pageContent.login();
-  }
-
-  function handleClick(curItem: { name: string; href: string; isActive: boolean }) {
-    showActive(curItem);
-    if (curItem.name === 'Logout') {
-      logout();
-    }
-  }
 </script>
 
 <div class="fixed flex h-screen w-[126px] min-w-[126px] flex-col items-center justify-center bg-bg-black">
   <img alt="iconLogo" src={iconLogo} class="absolute left-6 top-7 w-[57px]" />
   <div class="flex w-[100%] flex-col">
-    {#each navItems as navItem}
-      <a
-        on:click|preventDefault={() => handleClick(navItem)}
-        href={navItem.href}
-        class={` flex h-[100px] items-center justify-center text-sm font-bold ${
-          navItem.isActive && 'bg-bg-black-active shadow-blue-border'
-        }`}>{navItem.name}</a
-      >
-    {/each}
+    <NavItem href="/" isActive={url === '/'}>Home</NavItem>
+    <NavItem href="/faq" isActive={url === '/faq'}>FAQ's</NavItem>
+    {#if $isLoggedIn}
+      <NavItem href="/" onClick={logout}>Logout</NavItem>
+    {/if}
   </div>
 </div>
--- src/lib/stores/userStore.ts
+++ src/lib/stores/userStore.ts
@@ -1,4 +1,15 @@
 import { writable } from 'svelte/store';
 import { Account } from './accountsStore';
+import { dotApi, isLoggedIn, storeChainInfo } from '../stores';
+import { defaultDotApi } from '../storeTypes';
+import { pageContent } from '../stores/pageContentStore';
 
 export const user = writable<Account>(new Account());
+
+export const logout = () => {
+  user.set(new Account());
+  dotApi.set(defaultDotApi);
+  isLoggedIn.set(false);
+  storeChainInfo.set({ connected: false, blockNumber: 0n, epochNumber: 0n, token: '' });
+  pageContent.login();
+};
