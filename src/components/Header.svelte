fb43a31ce55f62711c507b6fcb24508bed74d7a4
Wil Wade <wil.wade@amplica.io>
2024-03-20 16:11:27 -0400

    Swap logo and wave for svgs

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,11 @@
 <script>
-  import logo from '$lib/assets/logo.png';
+  import Logo from './Logo.svelte';
   import ConnectionStatus from './ConnectionStatus.svelte';
 </script>
 
 <header class="mb-14 flex items-start justify-between">
-  <div>
-    <img alt="The project logo" src={logo} class="h-11" />
+  <div class="pt-3">
+    <div class="max-w-56 pb-1 pl-3"><Logo /></div>
     <p class="text-md pl-3 font-semibold tracking-wider">Provider Dashboard</p>
   </div>
   <ConnectionStatus />
