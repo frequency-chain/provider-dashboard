57cf77ef53a75eb387974e42450cab903b358570
Wil Wade <wil.wade@amplica.io>
2024-03-26 12:04:23 -0400

    Fixup accounts to have a better display and derive instead of having four stores

--- src/lib/stores/accountsStore.ts
+++ src/lib/stores/accountsStore.ts
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
+import { derived, writable, type Readable } from 'svelte/store';
 import { Keyring, type ApiPromise } from '@polkadot/api';
 import type { web3Enable, web3Accounts } from '@polkadot/extension-dapp';
 import { getMsaInfo } from '$lib/polkadotApi';
@@ -19,6 +19,7 @@ export class Account {
   msaId?: number;
   isProvider: boolean = false;
   providerName?: string;
+  display?: string;
 
   constructor() {
     this.address = '';
@@ -27,10 +28,25 @@ export class Account {
 
 export type Accounts = Map<SS58Address, Readonly<Account>>;
 
-export const providerAccountsStore = writable<Accounts>(new Map<SS58Address, Readonly<Account>>());
-export const nonProviderAccountsStore = writable<Accounts>(new Map<SS58Address, Readonly<Account>>());
-export const unusedKeyAccountsStore = writable<Accounts>(new Map<SS58Address, Readonly<Account>>());
+// Helper functions to filter all accounts to just the accounts for that store
+const isProvider = ([_key, account]: [string, Account]) => account.isProvider;
+const isNotProviderMsa = ([_key, account]: [string, Account]) => !account.isProvider;
+const isUnused = ([_key, account]: [string, Account]) => !account.isProvider && !account.msaId;
+
+// Stores all accounts
 export const allAccountsStore = writable<Accounts>(new Map<SS58Address, Readonly<Account>>());
+// Filtered down to just provider accounts
+export const providerAccountsStore = derived<Readable<Accounts>, Accounts>(allAccountsStore, (all, setFn) => {
+  setFn(new Map([...all.entries()].filter(isProvider)));
+});
+// These accounts don't even have an MSA
+export const unusedKeyAccountsStore = derived<Readable<Accounts>, Accounts>(allAccountsStore, (all, setFn) => {
+  setFn(new Map([...all.entries()].filter(isUnused)));
+});
+// These accounts have an MSA, but are not a provider
+export const nonProviderAccountsStore = derived<Readable<Accounts>, Accounts>(allAccountsStore, (all, setFn) => {
+  setFn(new Map([...all.entries()].filter(isNotProviderMsa)));
+});
 
 export async function fetchAccountsForNetwork(
   selectedNetwork: NetworkInfo,
@@ -40,9 +56,6 @@ export async function fetchAccountsForNetwork(
 ): Promise<void> {
   console.log('fetchAccountsForNetwork() - ', selectedNetwork);
 
-  const providerAccounts: Accounts = new Map<SS58Address, Account>();
-  const nonProviderAccounts: Accounts = new Map<SS58Address, Account>();
-  const unusedKeyAccounts: Accounts = new Map<SS58Address, Account>();
   const allAccounts: Accounts = new Map<SS58Address, Account>();
 
   // If the network is localhost, add the default test accounts for the chain
@@ -60,56 +73,42 @@ export async function fetchAccountsForNetwork(
         account.keyringPair = keyRingPair;
         account.isProvider = msaInfo.isProvider;
         account.providerName = providerNameToHuman(msaInfo.providerName);
+        account.display = accountName;
         allAccounts.set(account.address, account);
-        if (account.isProvider) {
-          providerAccounts.set(account.address, account);
-        } else {
-          if (account.msaId === 0) {
-            unusedKeyAccounts.set(account.address, account);
-          }
-          nonProviderAccounts.set(account.address, account);
-        }
       })
     );
   }
-  // Check if the Polkadot{.js} wallet extension is installed.
-  if (isFunction(thisWeb3Accounts) && isFunction(thisWeb3Enable)) {
-    const extensions = await thisWeb3Enable('Frequency parachain provider dashboard');
-    if (!extensions || !extensions.length) {
-      alert('Polkadot{.js} extension not found; please install it first.');
-      throw new Error('Polkadot{.js} extension not found; please install it first.');
+  try {
+    // Check if the Polkadot{.js} wallet extension is installed.
+    if (isFunction(thisWeb3Accounts) && isFunction(thisWeb3Enable)) {
+      const extensions = await thisWeb3Enable('Frequency parachain provider dashboard');
+      if (!extensions || !extensions.length) {
+        alert('Polkadot{.js} extension not found; please install it first.');
+        throw new Error('Polkadot{.js} extension not found; please install it first.');
+      }
+
+      // If so, add the wallet accounts for the selected network (chain)
+      const walletAccounts = await thisWeb3Accounts();
+      await Promise.all(
+        walletAccounts.map(async (walletAccount: InjectedAccountWithMeta) => {
+          // include only the accounts allowed for this chain
+          if (!walletAccount.meta.genesisHash || selectedNetwork.genesisHash === walletAccount.meta.genesisHash) {
+            const account = new Account();
+            account.network = selectedNetwork;
+            account.address = walletAccount.address;
+            account.injectedAccount = walletAccount;
+            const msaInfo: MsaInfo = await getMsaInfo(apiPromise, account.address);
+            account.msaId = msaInfo.msaId;
+            account.isProvider = msaInfo.isProvider;
+            account.providerName = providerNameToHuman(msaInfo.providerName);
+            account.display = walletAccount.meta.name;
+            allAccounts.set(account.address, account);
+          }
+        })
+      );
     }
-
-    // If so, add the wallet accounts for the selected network (chain)
-    const walletAccounts = await thisWeb3Accounts();
-    await Promise.all(
-      walletAccounts.map(async (walletAccount: InjectedAccountWithMeta) => {
-        // include only the accounts allowed for this chain
-        if (!walletAccount.meta.genesisHash || selectedNetwork.genesisHash === walletAccount.meta.genesisHash) {
-          const account = new Account();
-          account.network = selectedNetwork;
-          account.address = walletAccount.address;
-          account.injectedAccount = walletAccount;
-          const msaInfo: MsaInfo = await getMsaInfo(apiPromise, account.address);
-          account.msaId = msaInfo.msaId;
-          account.isProvider = msaInfo.isProvider;
-          account.providerName = providerNameToHuman(msaInfo.providerName);
-          allAccounts.set(account.address, account);
-          if (account.isProvider) {
-            providerAccounts.set(account.address, account);
-          } else {
-            if (account.msaId === 0) {
-              unusedKeyAccounts.set(account.address, account);
-            }
-            nonProviderAccounts.set(account.address, account);
-          }
-        }
-      })
-    );
+  } catch (e) {
+    console.error('Unable to load extension accounts', e);
   }
-
-  providerAccountsStore.set(providerAccounts);
-  nonProviderAccountsStore.set(nonProviderAccounts);
-  unusedKeyAccountsStore.set(unusedKeyAccounts);
   allAccountsStore.set(allAccounts);
 }
