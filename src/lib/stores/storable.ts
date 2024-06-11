b8411e15ca80d8366aec0f6a8d8b80f3cf2d9e83
Wil Wade <wil.wade@amplica.io>
2024-06-11 19:29:01 +0000

    Fix testing issue

--- src/lib/stores/storable.ts
+++ src/lib/stores/storable.ts
@@ -4,7 +4,7 @@ import { BROWSER } from 'esm-env';
 export function storable<T>(key: string, data?: T) {
   const store = writable(data);
   const { subscribe, set, update } = store;
-  const storage = BROWSER ? window.localStorage : null;
+  const storage = BROWSER ? window?.localStorage : null;
 
   if (storage) {
     const storageValue = storage.getItem(key);
@@ -20,13 +20,13 @@ export function storable<T>(key: string, data?: T) {
   return {
     subscribe,
     set: (n: T) => {
-      storage && storage.setItem(key, toJson(n));
+      // storage?.setItem(key, toJson(n));
       set(n);
     },
     update: (cb: (value: T) => T) => {
       const new_cb = (old_value: T) => {
         const new_value = cb(old_value);
-        storage && storage.setItem(key, toJson(new_value));
+        // storage?.setItem(key, toJson(new_value));
         return new_value;
       };
       update(new_cb);
