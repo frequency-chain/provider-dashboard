f879188a17b8ea82bc83f628ce84f15571eeeac0
shannonwells <shannonwells@users.noreply.github.com>
2024-09-26 15:21:26 -0700

    please the linter

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { base } from "$app/paths";
-  import logo from "$lib/assets/logo.svg";
- </script>
+  import { base } from '$app/paths';
+  import logo from '$lib/assets/logo.svg';
+</script>
 
 <a href={`${base}/`}><img src={logo} alt="logo" /></a>
