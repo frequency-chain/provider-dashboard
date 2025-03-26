01e8d2752f4361aae67f9544cc4ad233284653b2
Claire Olmstead Clark <43625033+claireclark1@users.noreply.github.com>
2025-03-26 12:44:34 -0700

    request to be a provider (#206)
    The goal of this PR is to request to become to a provider using the
    proppose to be a provider extrinsic.
    
    Closes #205
    
    - repurpose request to be a provider component
    - improve logging
    
    - [x] PR Self-Review and Commenting
    - [na] Docs updated
    - [na] Tests added
    
    1. Clone the pr branch
    2. Run through the mainnet request to become provider flow if you have
    FRQY in your account
    3. If not, change the condition in `BecomeAProvider` to testnet and run
    the mock flow through testnet
    4. Run success cases (has enough tokens, etc.)
    5. Run failure cases (has already submitted a request, etc.)
    
    pt.1
    
    https://github.com/user-attachments/assets/bdf7aa31-d464-4ef9-8682-6a8d83f147ca
    pt.2
    
    https://github.com/user-attachments/assets/bc7d7528-a7e1-488c-8fd2-b659811835e4
    pt.3
    
    https://github.com/user-attachments/assets/2a770202-12fb-4384-a7bf-157acef4771d

--- src/components/BackHomeButton.svelte
+++ src/components/BackHomeButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
+  import { logout } from '$lib/stores';
   let { cancelText = 'Cancel', classes = '' } = $props();
 </script>
 
-<a href="/" class={classes} data-testid="back-home">{cancelText}</a>
+<a href="/" class={`btn-no-fill ${classes}`} data-testid="back-home" on:click={logout}>{cancelText}</a>
--- src/components/BecomeAProvider.svelte
+++ src/components/BecomeAProvider.svelte
@@ -5,38 +5,44 @@
   import SelectNetworkAndAccount from './SelectNetworkAndAccount.svelte';
   import CreateMsa from './CreateMsa.svelte';
   import CreateProvider from './CreateProvider.svelte';
-  import EmailProviderRequest from './EmailProviderRequest.svelte';
   import { NetworkType } from '$lib/stores/networksStore';
   import BackHomeButton from '$components/BackHomeButton.svelte';
+  import RequestToBeProvider from '$components/RequestToBeProvider.svelte';
+
+  let hasRequestedToBeProvider = $state(false);
 </script>
 
-<BlockSection title="Become a Provider">
-  <form class="">
-    <SelectNetworkAndAccount
-      bind:newUser={$user}
-      accounts={$nonProviderAccountsStore}
-      accountSelectorTitle="Select an Account Id"
-      accountSelectorPlaceholder="Select an Account Id"
-      noAccountsFoundErrorMsg="No accounts found.  Add an Account Id to your wallet."
-    />
-    {#if $user?.network != null && $user.network.id === NetworkType.MAINNET}
-      <EmailProviderRequest />
-    {:else if $user && $user?.address !== ''}
-      {#if $user?.msaId === 0}
-        <CreateMsa />
-      {:else}
-        <CreateProvider />
+<div id="become-a-provider" class="content-block column w-single-block">
+  <BlockSection title="Become a Provider">
+    <form class="column w-[320px]">
+      {#if hasRequestedToBeProvider === false}
+        <SelectNetworkAndAccount
+          bind:newUser={$user}
+          accounts={$nonProviderAccountsStore}
+          accountSelectorTitle="Select an Account Id"
+          accountSelectorPlaceholder="Select an Account Id"
+          noAccountsFoundErrorMsg="No accounts found.  Add an Account Id to your wallet."
+        />
       {/if}
-    {:else}
-      <BackHomeButton cancelText="Back" />
-    {/if}
-  </form>
-</BlockSection>
-<BlockSection title="More Info">
-  <div class="text-sm">
-    For developer and testing convenience, on Testnet, anyone with an MSA who wishes to become a Provider may simply
-    submit a createProvider transaction.<br /><br />This action will register the MSA Id that is controlled by the
-    selected Transaction Signing Address above. Any Account Id that has been added to the MSA can submit the
-    transaction.
-  </div>
-</BlockSection>
+      {#if $user && $user?.address !== ''}
+        {#if $user?.msaId === 0}
+          <CreateMsa />
+        {:else if $user?.network?.id === NetworkType.TESTNET_PASEO}
+          <RequestToBeProvider bind:hasRequestedToBeProvider />
+        {:else}
+          <CreateProvider />
+        {/if}
+      {:else}
+        <BackHomeButton cancelText="Back" />
+      {/if}
+    </form>
+  </BlockSection>
+  <BlockSection title="More Info">
+    <div class="text-sm">
+      For developer and testing convenience, on Testnet, anyone with an MSA who wishes to become a Provider may simply
+      submit a createProvider transaction.<br /><br />This action will register the MSA Id that is controlled by the
+      selected Transaction Signing Address above. Any Account Id that has been added to the MSA can submit the
+      transaction.
+    </div>
+  </BlockSection>
+</div>
