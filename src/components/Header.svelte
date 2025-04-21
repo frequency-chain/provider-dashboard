ba4552fcc8ed57c4d49115d5394b55375f5756ab
Claire Olmstead <olmsteadclaire@gmail.com>
2025-04-21 11:50:57 -0700

    swap header and footer logos to new ones. git rid of old footer

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
   import { isLoggedIn } from '$lib/stores';
-  import FrequencyLogo from './icons/FrequencyLogo.svelte';
   import ConnectionStatus from './ConnectionStatus.svelte';
-  import { Button } from '@frequency-chain/style-guide';
+  import { Button, Assets } from '@frequency-chain/style-guide';
 </script>
 
 <header class="mb-8 flex items-start justify-between gap-3">
   <div>
-    <div class="w-[180px] max-w-[180px] pb-1 pl-3 md:min-w-40">
-      <FrequencyLogo />
-    </div>
+    <Assets.FrequencyLogo class="w-[180px] max-w-[180px] pb-1 pl-3 md:min-w-40" />
     <p class="md:text-normal pl-3 text-sm font-semibold lg:tracking-wider">Provider Dashboard</p>
   </div>
   <ConnectionStatus />
