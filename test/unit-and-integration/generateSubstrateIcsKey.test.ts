c94913a6214d3479a901b12d629150495fdac3ae
Joe Caputo <joseph.caputo@projectliberty.io>
2026-05-07 13:58:33 -0400

    fix: formatting

--- test/unit-and-integration/generateSubstrateIcsKey.test.ts
+++ test/unit-and-integration/generateSubstrateIcsKey.test.ts
@@ -1,5 +1,5 @@
 import IcsApi, { IcsKeyType } from '@projectlibertylabs/ics-sdk';
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from 'vitest';
 import { generateSubstrateKeypair } from '$lib/generateSubstrateIcsKey';
 
 const icsSdk = new IcsApi();
@@ -14,7 +14,7 @@ describe('Native vs JS key generation', () => {
   it('should generate the same keypair', async () => {
     const [jsKeypair, mnemonic] = await generateSubstrateKeypair();
     const nativeSeed = icsSdk.deriveMasterSeedFromMnemonic(mnemonic);
-    const { publicKey, secretKey} = icsSdk.deriveUserKeyPair(nativeSeed, IcsKeyType.Ed25519);
+    const { publicKey, secretKey } = icsSdk.deriveUserKeyPair(nativeSeed, IcsKeyType.Ed25519);
     const nativeKeypair = { publicKey, secretKey };
 
     expect(jsKeypair.publicKey.length).toBe(nativeKeypair.publicKey.length);
